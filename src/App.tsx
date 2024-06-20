d78deed0cb200e48885f40cd533eb5f7b116f2ce
dmitry <yarovenkojob@gmail.com>
2024-06-20 17:47:28 +0300

    feat: add showing all comments by id

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import getCommentsRequest from "./api/comments/getCommentsRequest";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+type TComment = {
+    id: number;
+    created: Date;
+    text: string;
+    author: number;
+    parent: number | null;
+    likes: number;
 }
 
-export default App;
+export const App = () => {
+    const [commentsData, setCommentsData] = useState<any>({});
+    const [currentPage, setCurrentPage] = useState(1);
+
+    console.log(">>> commentsData :", commentsData);
+
+    useEffect(() => {
+        async function fetchComments() {
+            const response = await getCommentsRequest(currentPage);
+            console.log(">>> response :", response);
+            setCommentsData(response);
+        }
+
+        fetchComments();
+    }, [currentPage]);
+
+    const changePage = () => {
+        setCurrentPage(prevPage => prevPage + 1);
+        console.log(">>> changePage :");
+    };
+
+
+    let comments = commentsData.data;
+
+    if (commentsData.data) {
+        comments = commentsData.data.sort((a: TComment, b: TComment) => a.id - b.id);
+        console.log(">>> comments  sort:", comments);
+
+    }
+
+    return (
+        <div>
+            {comments && comments.map((comment: TComment) => (
+                <div key={comment.id}>
+                    <h2 style={{color: "green"}}>author: {comment.author}</h2>
+                    <h3>id: {comment.id}</h3>
+                    <p>text: {comment.text}</p>
+                    <h3>likes: {comment.likes}</h3>
+                    <h3>created: {new Date(comment.created).toDateString()}</h3>
+                    <p style={{color: "red"}}>parent: {comment.parent}</p>
+
+                    ************************
+                </div>
+            ))}
+
+            <button onClick={changePage}>Загрузить следующие комментарии</button>
+        </div>
+    );
+};
